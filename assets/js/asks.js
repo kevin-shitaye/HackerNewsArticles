23f631d51e4e14b9083af8856c715e01e113a58e
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-03-02 22:26:00 +0300

    got ui for asks page

---
+++ assets/js/asks.js
@@ -0,0 +1,6 @@
+// UIs
+const main_author = document.querySelector("#main_by")
+const main_time = document.querySelector("#main_time")
+const main_topic = document.querySelector("#main_topic")
+const main_text = document.querySelector("#main_text")
+const comments_section = document.querySelector('#comments_section')
