5ca9c0816afcc3d2f058bc5d71f6599b42e90b30
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-03-02 22:30:52 +0300

    function getfromurl and getitem add for asks page

--- assets/js/asks.js
+++ assets/js/asks.js
@@ -4,3 +4,17 @@ const main_time = document.querySelector("#main_time")
 const main_topic = document.querySelector("#main_topic")
 const main_text = document.querySelector("#main_text")
 const comments_section = document.querySelector('#comments_section')
+
+function getIdFromURL() {
+    let parameter = new URLSearchParams(window.location.search)
+    return parameter.get("id")
+}
+
+async function get_item(id) {
+
+    let response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
+    let data = await response.json();
+
+    return data
+
+}
