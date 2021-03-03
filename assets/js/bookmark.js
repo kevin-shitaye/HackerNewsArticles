c0fc6ced992cc2029ad999b8ff7bfb41810e89c5
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-03-03 22:30:39 +0300

    working on displaying bookmarks

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -22,3 +22,42 @@ async function get_item(id) {
     return data
 
 }
+
+function displayBookmarked() {
+    articles_div.innerHTML = ""
+    let bookmarks = localStorage.getItem("bookmarks")
+    if (bookmarks && JSON.parse(bookmarks).length != 0) {
+        let list_of_ids = JSON.parse(bookmarks)
+
+
+        spinner.style.visibility = "visible";
+
+        list_of_ids.forEach(id => {
+            get_item(id).then(data =>{
+                spinner.style.visibility = "hidden";
+                if (data != null) {
+                    let date = moment.unix(data.time).fromNow()
+
+                    let text = ""
+                    if (data.text) {
+                        text = data.text.slice(0, 80)
+                    }
+                    const article = document.createElement('div')
+                    article.innerHTML = ` 
+                    <article class="article">
+                    <a href="${data.url}" target="_blank"><h2>${data.title}</h2></a> 
+                    <div class="bookmark_btn" id="btn${data.id}"><i class="fas fa-bookmark"></i></div>
+                    <p>${text}...</p>
+                    <p><b>By</b>: <i>${data.by} |</i>  
+                    <i class="far fa-star">Score: ${data.score} |</i> 
+                    <i>${date}</i></p>
+                    </article>
+                    `
+
+                    
+
+
+                    // adding date to the article for sorting
+                    article.firstElementChild.dataset.date = data.time
+                    articles_div.appendChild(article)
+                    articles.appendChild(articles_div)
